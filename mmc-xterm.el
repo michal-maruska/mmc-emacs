dc0b3dda74c3eb1c37cb1d7a704db2957b4514da
Michal Maruska <mmaruska@gmail.com>
2011-01-31 16:00:11 +0100

    finally drop the <xterm> buffers

--- mmc-xterm.el
+++ mmc-xterm.el
@@ -2,7 +2,7 @@
 (defun xterm (&optional arg command title)
   "open an xterm on the DISPLAY where FRAME is. With prefix, ask for the DISPLAY."
   (interactive "P")
-  (let ((buffer (generate-new-buffer "xterm"))
+  (let (; (buffer (generate-new-buffer "xterm"))
         (display
          (if running-xemacs
              (frame-property (selected-frame) 'display "0:0")
@@ -15,36 +15,46 @@
           (setenv "INSIDE_EMACS" nil)
           (with-display display
             (save-window-excursion
-              (shell-command
-               (concat
-                (mapconcat
-                 'identity
-                 (list
-                  "rxvt"
-                  "-geometry" "100x60"
-                  ;; no scrollbar!
-                  "+sb"
-                  ;; "-sl" "500"
-                  ;; login shell? NO
-                  "+ls"
-                  "-j"
-                  ;; "-ls"
-                  "-fn" "'-*-lucidatypewriter-medium-r-normal-*-20-*-*-*-*-*-*'"
-                  ;"-title" "emacs"
+              ;;(shell-command
+	      (apply
+	       'start-process
+	       "xterm"
+	       nil
+
+	       "rxvt"
+					;(concat
+					;(mapconcat
+					; 'identity
+
+					;(list
+	       ;;"rxvt"
+	       "-geometry" "100x60"
+	       ;; no scrollbar!
+	       "+sb"
+	       ;; "-sl" "500"
+	       ;; login shell? NO
+	       "+ls"
+	       "-j"
+	       ;; "-ls"
+	       "-fn"
+	       ;; fixme: duplicated from ~/.Xdefaults
+	       "-*-lucidatypewriter-medium-r-normal-*-18-*-*-*-*-*-*"
+					;"-title" "emacs"
                   
 
-                  ;; Old:
-                  ;"xterm"; "/usr/local/bin/xterm" ;X11R6
-                  ;"+sb -sl 5000"
-                  ;"-geometry 90x52+10+0"
+	       ;; Old:
+					;"xterm"; "/usr/local/bin/xterm" ;X11R6
+					;"+sb -sl 5000"
+					;"-geometry 90x52+10+0"
                                         ;		  "-j -ls "
                                         ;		  "-fn '-*-lucidatypewriter-medium-r-normal-*-20-*-*-*-*-*-*'"
                                         ;		  "-bg black"
                                         ;		  "-fg wheat"
-                  ;(format "-title \"%s\"" (or title "exterm"))
-                  (if command (format "-e %s" command) "")
-                  ) " ")
-                "&") buffer))
+					;(format "-title \"%s\"" (or title "exterm"))
+	       (if command
+		   (list "-e" command)
+		 ())))
+	       ;) ;;" ")
             ;; (bury-buffer)
             ;; put the buffer to auto-desctroy list...
             ;; Xterm survives, but  rxvt does not!
