86bd7654f6d77900746b7aeb072f23d417f46e9a
Michal Maruska <mmaruska@gmail.com>
2011-05-22 22:28:59 +0200

    import   find-function-search-for-symbol

--- mmc-simple.el
+++ mmc-simple.el
@@ -1149,3 +1149,60 @@ If the current buffer now contains an empty file that you just visited
   (interactive)
   (let ((file (buffer-file-name (current-buffer))))
     (byte-compile-file file 't)))
+
+
+(defun find-function-search-for-symbol (symbol type library)
+  "Search for SYMBOL's definition of type TYPE in LIBRARY.
+Visit the library in a buffer, and return a cons cell (BUFFER . POSITION),
+or just (BUFFER . nil) if the definition can't be found in the file.
+
+If TYPE is nil, look for a function definition.
+Otherwise, TYPE specifies the kind of definition,
+and it is interpreted via `find-function-regexp-alist'.
+The search is done in the source for library LIBRARY."
+  (if (null library)
+      (error "Don't know where `%s' is defined" symbol))
+  ;; Some functions are defined as part of the construct
+  ;; that defines something else.
+  (while (and (symbolp symbol) (get symbol 'definition-name))
+    (setq symbol (get symbol 'definition-name)))
+  (if (string-match "\\`src/\\(.*\\.\\(c\\|m\\)\\)\\'" library)
+      (find-function-C-source symbol (match-string 1 library) type)
+    (when (string-match "\\.el\\(c\\)\\'" library)
+      (setq library (substring library 0 (match-beginning 1))))
+    ;; Strip extension from .emacs.el to make sure symbol is searched in
+    ;; .emacs too.
+    (when (string-match "\\.emacs\\(.el\\)" library)
+      (setq library (substring library 0 (match-beginning 1))))
+    (let* ((filename (find-library-name library))
+	   (regexp-symbol (cdr (assq type find-function-regexp-alist))))
+      (with-current-buffer (find-file-noselect filename)
+	(let ((regexp (format (symbol-value regexp-symbol)
+			      ;; Entry for ` (backquote) macro in loaddefs.el,
+			      ;; (defalias (quote \`)..., has a \ but
+			      ;; (symbol-name symbol) doesn't.  Add an
+			      ;; optional \ to catch this.
+			      (concat "\\\\?"
+				      (regexp-quote (symbol-name symbol)))))
+	      (case-fold-search))
+	  (with-syntax-table emacs-lisp-mode-syntax-table
+	    (goto-char (point-min))
+	    (if (or (re-search-forward regexp nil t)
+                    ;; `regexp' matches definitions using known forms like
+                    ;; `defun', or `defvar'.  But some functions/variables
+                    ;; are defined using special macros (or functions), so
+                    ;; if `regexp' can't find the definition, we look for
+                    ;; something of the form "(SOMETHING <symbol> ...)".
+                    ;; This fails to distinguish function definitions from
+                    ;; variable declarations (or even uses thereof), but is
+                    ;; a good pragmatic fallback.
+		    (re-search-forward
+		     (concat "^([^ ]+" find-function-space-re "['(]?"
+			     (regexp-quote (symbol-name symbol))
+			     "\\_>")
+		     nil t))
+		(progn
+		  (beginning-of-line)
+		  (cons (current-buffer) (point)))
+	      (cons (current-buffer) nil))))))))
+
