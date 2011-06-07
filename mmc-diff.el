b31523b3e2a456d5225164db7b8373a773914094
Michal Maruska <mmaruska@gmail.com>
2011-06-07 16:58:11 +0200

    start to hightlight in diff-mode

--- mmc-diff.el
+++ mmc-diff.el
@@ -18,8 +18,49 @@
 (define-key diff-mode-map "\C-c\C-c" 'diff-2-ediff)
 (define-key diff-mode-map "\C-c\C-e" 'diff-goto-source)
 
-(set-face-background 'diff-file-header-face "red")
-(set-face-foreground 'diff-file-header-face "yellow")
+
+; (set-face-foreground 'diff-added (face-foreground 'magit-diff-add))
+;(set-face-foreground 'diff-removed (face-foreground 'magit-diff-del))
+;; (face-background 'magit-diff-add)
+; (face-foreground 'magit-diff-add)
+;(set-face-foreground 'diff-file-header-face "yellow")
+
+
+
+(defun fast-diff-mode ()
+  ""
+  (interactive)
+  (setq buffer-read-only 't))
+
+(defface diff-overlay-face nil "")
+
+(defun diff-post-command-hook ()
+  ;(magit-correct-point-after-command)
+  (fast-diff-mode-highlight))
+
+(add-hook 'post-command-hook #'diff-post-command-hook t t)
+;; (set-face-background 'diff-overlay-face "gray9")
+
+
+(defvar fast-diff-highlight-overlay nil)
+(defun fast-diff-mode-highlight ()
+  ""
+  (interactive)
+  (save-excursion
+    (let* ((start (progn (diff-beginning-of-hunk 'try-harder) (point)))
+	   (end (progn (diff-end-of-hunk) (point))))
+      (if (and start end)
+	  (if fast-diff-highlight-overlay
+	      (move-overlay fast-diff-highlight-overlay
+			    start end)
+	    (setq fast-diff-highlight-overlay
+		  (make-overlay start end)
+		  ;;(current-buffer)
+		  )
+	    (overlay-put fast-diff-highlight-overlay 'face 'diff-overlay-face)))
+      ;;priority'
+					;(overlay-put overlay 'backtround "dim gray")
+      )))
 
 
 (provide 'mmc-diff)
