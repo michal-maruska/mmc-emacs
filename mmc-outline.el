9e4a3ab7ed2a354916772e6df2559665b4274318
Michal Maruska <mmaruska@gmail.com>
2012-06-03 17:44:52 +0200

    fix BC -- first 2

--- mmc-outline.el
+++ mmc-outline.el
@@ -1,5 +1,22 @@
 ;;; my-outline.el --- key-bindings
 
+;; (require 'outline)
+(eval-and-compile
+  (if running-xemacs
+      (progn
+	;;(require 'outline "/usr/share/emacs/21.3/lisp/textmodes/outline.el")
+	(require 'outline)
+	;;(load "/usr/share/emacs/21.3/lisp/textmodes/outline.el")
+	(define-minor-mode outline-minor-mode
+	  "Toggle Outline minor mode.
+With arg, turn Outline minor mode on if arg is positive, off otherwise.
+See the command `outline-mode' for more information on this mode."
+	  nil " Outl" (list (cons [menu-bar] outline-mode-menu-bar-map)
+			    (cons outline-minor-mode-prefix
+				  outline-mode-prefix-map))
+	  ))
+    (require 'outline)))
+
 
 ;; http://ruska.dyndns.org/comp/emacs/local/my-outline.el
 ;; allout.el
@@ -41,22 +58,6 @@
 
 
 ;(keymapp outline-minor-mode-map)
-
-;; (require 'outline)
-(eval-and-compile
-  (if running-xemacs
-      (progn
-	;;(require 'outline "/usr/share/emacs/21.3/lisp/textmodes/outline.el")
-	(require 'outline)
-	;;(load "/usr/share/emacs/21.3/lisp/textmodes/outline.el")
-	(define-minor-mode outline-minor-mode
-	  "Toggle Outline minor mode.
-With arg, turn Outline minor mode on if arg is positive, off otherwise.
-See the command `outline-mode' for more information on this mode."
-	  nil " Outl" (list (cons [menu-bar] outline-mode-menu-bar-map)
-			    (cons outline-minor-mode-prefix outline-mode-prefix-map))
-	  ))
-    (require 'outline)))
 
 (unless
     (lookup-key outline-minor-mode-map (kbd "\C-c\C-d"))
