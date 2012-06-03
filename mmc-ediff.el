858de2a6e6ada50a52d2122bb9c2ca662a196869
Michal Maruska <mmaruska@gmail.com>
2012-06-03 15:08:15 +0200

    ediff:  divide into sections. Document my functions.

--- mmc-ediff.el
+++ mmc-ediff.el
@@ -8,33 +8,39 @@
       ;ediff-setup-windows-plain
       )
 
-;; (require 'ediff)
-
 ;(setq emerge-diff-options "--ignore-all-space")
-
-
-
-
-;;; Description:
-;; Sometimes i must (lose synchronization with my co-workers) revert the buffer, but don't know
-;; whether i made some changes:
-
-(defcustom my-ediff-dir "/tmp/emacs/ediff/"
-  "Dir where i store temporary copy of the buffer, when ... to the file"
-  :group 'ediff
-  :type 'filename);; directory
-
-
 ;; ignore space
 (setq ediff-diff-options "-w")
 (setq ediff-filtering-regexp-history '("[^~]$"))
 (setq ediff-auto-refine-limit 50000)
 
 
+(eval-after-load "ediff"
+  '(progn
+     (setcdr
+      (car ediff-control-frame-parameters) nil)
+
+     (setq
+      ediff-control-frame-parameters
+      (aput
+       'ediff-control-frame-parameters 'auto-raise ))))
+
+
+
+
+;;; Description:
+;; Sometimes I must revert the buffer, but don't know
+;; whether I made some changes: (lose synchronization with my co-workers)
+
+
+(defcustom my-ediff-dir "/tmp/emacs/ediff/"
+  "Dir where i store temporary copy of the buffer, when ... to the file"
+  :group 'ediff
+  :type 'filename);; directory
 
 ;; But w/ cvs !!
 (defun my-ediff (buffer prefix)
-  "diff the current buffer with its image on the disk"
+  "diff the CURRENT buffer with its image on the disk"
   (interactive "bEdiff buffer with its file: \np")
   (condition-case nil
       (with-current-buffer buffer
@@ -50,16 +56,16 @@
 
 
 
-;; i want the stuff accessible from Global keymap:
+;; I want the stuff accessible from Global keymap:
 (defconst ediff-keymap (make-sparse-keymap) "")
 
 (let ((map ediff-keymap))
-  (global-set-key [(control ?E)] map)	;shift
   (define-key map "d" 'edirs)
   (define-key map "b" 'my-ediff-buffers)
   ;(define-key map "b" 'ebuffers)
   (define-key map "f" 'ediff))
 
+(global-set-key [(control ?E)] 'ediff-map)
 
 
 ;(frame-first-window
@@ -76,43 +82,43 @@
 
 
 
-;;; ------------------------------------------------- patch -------------------------
-;; (require 'ediff)
+;;; ---------- patch -------------------------
 
 ;; [06 gen 05]   i want to start it w/ 2 `dired' buffers/windows in the frame.
 ;; And the prompt should be aware of it.
-(eval-after-load "ediff"
-  '(defun ediff-directories (dir1 dir2 regexp)
-     "Run Ediff on a pair of directories, DIR1 and DIR2, comparing files that have
+
+;; Overriding standard function:
+(defun ediff-directories (dir1 dir2 regexp)
+  "Run Ediff on a pair of directories, DIR1 and DIR2, comparing files that have
 the same name in both.  The third argument, REGEXP, is a regular expression
 that can be used to filter out certain file names."
-     (interactive
-      (let ((dir-A
-             default-directory
+  (interactive
+   (let ((dir-A
+	  default-directory
                                         ;(ediff-get-default-directory-name)
-             )
-            (dir-B
-             ;; realign buffers so that two visible bufs will be
-             ;; at the top
-             (save-window-excursion
-               (other-window 1)
-               default-directory        ;(ediff-get-default-directory-name)
-               ))
-            f)
-        (list (setq f (ediff-read-file-name "Directory A to compare:" dir-A nil))
-              (ediff-read-file-name "Directory B to compare:"
-                                    ;; '(if ediff-use-last-dir
-                                    ;; 				     ediff-last-dir-B
-                                    ;;                                     (ediff-strip-last-dir f))
-                                    dir-B
-                                    nil)
-              (read-string "Filter through regular expression: "
-                           nil 'ediff-filtering-regexp-history)
-              )))
-     (ediff-directories-internal
-      dir1 dir2 nil regexp 'ediff-files 'ediff-directories)))
+	  )
+	 (dir-B
+	  ;; realign buffers so that two visible bufs will be
+	  ;; at the top
+	  (save-window-excursion
+	    (other-window 1)
+	    default-directory		;(ediff-get-default-directory-name)
+	    ))
+	 f)
+     (list (setq f (ediff-read-file-name "Directory A to compare:" dir-A nil))
+	   (ediff-read-file-name "Directory B to compare:"
+				 ;; '(if ediff-use-last-dir
+				 ;; 				     ediff-last-dir-B
+				 ;;                                     (ediff-strip-last-dir f))
+				 dir-B
+				 nil)
+	   (read-string "Filter through regular expression: "
+			nil 'ediff-filtering-regexp-history)
+	   )))
+  (ediff-directories-internal
+   dir1 dir2 nil regexp 'ediff-files 'ediff-directories))
 
-;;; ----------------------------------------------------------end patch -------------------
+;;; --------------end patch -------------------
 
 
 
@@ -132,15 +138,15 @@ that can be used to filter out certain file names."
     "~/xfree/xpatches/medved-plugin/"
 
     ;;"/linux/13/x/xfree86/build-medved/";; programs/Xserver/
-
-
-
     ;"/p/xfree-4.3.99.901-r4/work/"
-
     ;"/tmp/xpatches/xpatches/medved/"
     ))
+
+
 (defun ediff-x (filename)
-  ""
+  "run `ediff' on file & its companion.
+Filename is read (by `ffap'), and companion is searched for in
+a list of diretories ... see `ediff-x-roots'."
   (interactive (list (thing-at-point 'filename)))
   (ediff
    (compose-path  (car ediff-x-roots) filename)
@@ -148,9 +154,9 @@ that can be used to filter out certain file names."
   ))
 
 
-;; boundp
 
 (when nil
+  ;; Experiments with my feature
   (when
       (functionp 'set-frame-group-leader)
     (setq ediff-multiframe 't)
@@ -166,14 +172,5 @@ that can be used to filter out certain file names."
       (set-frame-group-leader ctl-frame base-frame-id))
   ))
 
-(eval-after-load "ediff"
-  '(progn
-     (setcdr
-      (car ediff-control-frame-parameters) nil)
-
-     (setq
-      ediff-control-frame-parameters
-      (aput
-       'ediff-control-frame-parameters 'auto-raise ))))
 
 (provide 'mmc-ediff)
