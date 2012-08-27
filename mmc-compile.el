8186b66040de0501578ecc1c943835fc4d3a8425
Michal Maruska <mmaruska@gmail.com>
2012-08-27 13:14:51 +0200

    grep don't descend into the .git directory

--- mmc-compile.el
+++ mmc-compile.el
@@ -6,7 +6,7 @@
 
 (require 'compile)                      ;grep
 ;; fixme: how to test if the GREP supports this...?
-(setq grep-command "grep --directories=recurse --no-recurse-symlinks --exclude='*~' -i -n -e ")
+(setq grep-command "grep --directories=recurse --no-recurse-symlinks --exclude='*~' --exclude-dir=.git -i -n -e ")
 (setq grep-program  (if emacs-22
                         "grep"
 		      "grep --directories=recurse --exclude='*~' --exclude=semantic.cache -i "))
