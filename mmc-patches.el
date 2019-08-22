bd73312a0256575b2634e90e2543d60f15db4fc0
Michal Maruska <mmaruska@gmail.com>
2019-08-22 07:42:48 +0200

    patch cmake-indent to indent the closing ) on a new-line _back_.
    Change-Id: I563b25c5fa7e720d451ecfb6bfddc6587560fe7e

--- mmc-patches.el
+++ mmc-patches.el
@@ -456,4 +456,50 @@ Record command in `command-history' if optional RECORD is non-nil."
             (set-buffer-major-mode buf))
         (ido-visit-buffer buf method t))))))
 
+;; cmake:
+(require 'cmake-mode)
+(defconst cmake-regex-close-paren
+  (rx-to-string `(and bol (* space) ,cmake-regex-paren-right)))
+
+(defun cmake-indent ()
+  "Indent current line as CMake code."
+  (interactive)
+  (unless (cmake-line-starts-inside-string)
+    (if (bobp)
+        (cmake-indent-line-to 0)
+      (let (cur-indent)
+        (save-excursion
+          (beginning-of-line)
+          (let ((point-start (point))
+                (case-fold-search t)  ;; case-insensitive
+                token)
+            ; Search back for the last indented line.
+            (cmake-find-last-indented-line)
+            ; Start with the indentation on this line.
+            (setq cur-indent (current-indentation))
+            ; Search forward counting tokens that adjust indentation.
+            (while (re-search-forward cmake-regex-token point-start t)
+              (setq token (match-string 0))
+              (when (or (string-match (concat "^" cmake-regex-paren-left "$") token)
+                        (and (string-match cmake-regex-block-open token)
+                             (looking-at (concat "[ \t]*" cmake-regex-paren-left))))
+                (setq cur-indent (+ cur-indent cmake-tab-width)))
+              (when (string-match (concat "^" cmake-regex-paren-right "$") token)
+                (setq cur-indent (- cur-indent cmake-tab-width)))
+              )
+            (goto-char point-start)
+            ;; If next token closes the block, decrease indentation
+            (when (or (looking-at cmake-regex-close)
+                      ;; mmc: add another case:
+                      (looking-at cmake-regex-close-paren))
+              (setq cur-indent (- cur-indent cmake-tab-width)))
+            )
+          )
+        ; Indent this line by the amount selected.
+        (cmake-indent-line-to (max cur-indent 0))
+        )
+      )
+    )
+  )
+
 (provide 'mmc-patches)
