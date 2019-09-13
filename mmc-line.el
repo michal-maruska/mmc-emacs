cb6632cf83d9958b8a14637eaca65f26972388f2
Michal Maruska <mmaruska@gmail.com>
2019-09-13 06:56:47 +0200

    spaces
    Change-Id: I38cb06f4c616af0e757594a72f4f2ad316f4b2e9

--- mmc-line.el
+++ mmc-line.el
@@ -88,20 +88,20 @@
 ;; ~/.emacs:14
 
 (defadvice server-visit-files (before parse-numbers-in-lines
-				      (files proc &optional nowait) activate)
+                                      (files proc &optional nowait) activate)
   "looks for filenames like file:line or file:line:position and reparses
 name in such manner that position in file"
   (ad-set-arg 0
      (mapcar
       (lambda (fn)
-	(let ((name (car fn)))
-	  (if (string-match "^\\(.*?\\):\\([0-9]+\\)\\(?::\\([0-9]+\\)\\)?$"
-			    name)
-	      (cons
-	       (match-string 1 name)
-	       (cons (string-to-number (match-string 2 name))
-		     (string-to-number (or (match-string 3 name) ""))))
-	    fn)))
+        (let ((name (car fn)))
+          (if (string-match "^\\(.*?\\):\\([0-9]+\\)\\(?::\\([0-9]+\\)\\)?$"
+                            name)
+              (cons
+               (match-string 1 name)
+               (cons (string-to-number (match-string 2 name))
+                     (string-to-number (or (match-string 3 name) ""))))
+            fn)))
       files)))
 
 
