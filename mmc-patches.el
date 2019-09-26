cbc0687cfeb405b8eb4c76bed37e0c7c229e1884
Michal Maruska <mmaruska@gmail.com>
2019-09-26 21:52:07 +0200

    fix possible nil in grep-...
    Change-Id: I1585371401a441bfd0235d3e8c52b5a07c5e36af

--- mmc-patches.el
+++ mmc-patches.el
@@ -503,4 +503,43 @@ Record command in `command-history' if optional RECORD is non-nil."
     )
   )
 
+;; /usr/share/emacs/27.0.50/lisp/progmodes/grep.el
+;;; fix bug:
+(defun grep-default-command ()
+  "Compute the default grep command for \\[universal-argument] \\[grep] to offer."
+  (let ((tag-default (shell-quote-argument (grep-tag-default)))
+        ;; This a regexp to match single shell arguments.
+        ;; Could someone please add comments explaining it?
+        (sh-arg-re "\\(\\(?:\"\\(?:[^\"]\\|\\\\\"\\)+\"\\|'[^']+'\\|[^\"' \t\n]\\)+\\)")
+        (grep-default (or (car grep-history) grep-command)))
+    ;; In the default command, find the arg that specifies the pattern.
+    (when (or (string-match
+               (concat "[^ ]+\\s +\\(?:-[^ ]+\\s +\\)*"
+                       sh-arg-re "\\(\\s +\\(\\S +\\)\\)?")
+               grep-default)
+              ;; If the string is not yet complete.
+              (string-match "\\(\\)\\'" grep-default))
+      ;; Maybe we will replace the pattern with the default tag.
+      ;; But first, maybe replace the file name pattern.
+      (condition-case nil
+          (unless (or (not (stringp buffer-file-name))
+                      t ;; mmc: maruska!
+                      (when (match-beginning 2)
+                        (save-match-data
+                          (string-match
+                           (wildcard-to-regexp
+                            (file-name-nondirectory
+                             (match-string 3 grep-default)))
+                           (file-name-nondirectory buffer-file-name)))))
+
+            (setq grep-default (concat (substring grep-default
+                                                  0 (match-beginning 2))
+                                       " *."
+                                       (file-name-extension buffer-file-name))))
+        ;; In case wildcard-to-regexp gets an error
+        ;; from invalid data.
+        (error nil))
+      ;; Now replace the pattern with the default tag.
+      (replace-match tag-default t t grep-default 1))))
+
 (provide 'mmc-patches)
