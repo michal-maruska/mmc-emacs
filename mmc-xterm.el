115fadbc01ed29ac0e7333e83f2a3916e8a79f79
Michal Maruska <mmaruska@gmail.com>
2025-10-27 15:21:33 +0100

    xterm: use wezterm

--- mmc-xterm.el
+++ mmc-xterm.el
@@ -9,30 +9,38 @@
    (lambda ()
      (save-window-excursion
        ;;(shell-command
-       (apply
-        'start-process
-        "xterm"
-        nil
-        "rxvt"
+       (let ((args
+              '(
+                "rxvt"
+                "-geometry" "90x40"		;40 rows...
+                ;; no scrollbar!
+                "+sb"
+                ;; "-sl" "500"
+                ;; login shell? NO
+                "+ls"
+                "-j"
+                )))
+         (apply
+          'start-process
+          "xterm"
+          nil
+          "wezterm"
+          "--config"
+          "dpi=198"
+          "start"
+          "--cwd" default-directory
                                         ;(concat
                                         ;(mapconcat
                                         ; 'identity
 
                                         ;(list
-        ;;"rxvt"
-        "-geometry" "90x40"		;40 rows...
-        ;; no scrollbar!
-        "+sb"
-        ;; "-sl" "500"
-        ;; login shell? NO
-        "+ls"
-        "-j"
-        ;; "-ls"
-        ;; fixme: duplicated from ~/.Xdefaults
-        ;"-fn"
-        ;"-*-lucidatypewriter-medium-r-normal-*-18-*-*-*-*-*-*"
-        ;;"-title" "emacs"
-        ;; Old:
+          ;;"rxvt"
+          ;; "-ls"
+          ;; fixme: duplicated from ~/.Xdefaults
+                                        ;"-fn"
+                                        ;"-*-lucidatypewriter-medium-r-normal-*-18-*-*-*-*-*-*"
+          ;;"-title" "emacs"
+          ;; Old:
                                         ;"xterm"; "/usr/local/bin/xterm" ;X11R6
                                         ;"+sb -sl 5000"
                                         ;"-geometry 90x52+10+0"
@@ -41,15 +49,15 @@
                                         ;		  "-bg black"
                                         ;		  "-fg wheat"
                                         ;(format "-title \"%s\"" (or title "exterm"))
-        (if command
-            (list "-e" command)
-          ()))))
+          (if command
+              (list "-e" command)
+            ()))))
                                         ;) ;;" ")
    ;; (bury-buffer)
    ;; put the buffer to auto-desctroy list...
    ;; Xterm survives, but  rxvt does not!
                                         ;(run-with-timer 10 nil 'kill-buffer buffer) ; fixme: kill the bufer after ...
-     ))
+     )))
 
 
 ;; fixme: in mmc-keys?
