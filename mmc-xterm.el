c3438ab569f9fa787699a8c16c91b7e2a6d77a09
Michal Maruska <mmaruska@gmail.com>
2026-09-12 09:53:27 +0200

    xterm: use wezterm

--- mmc-xterm.el
+++ mmc-xterm.el
@@ -8,48 +8,23 @@
   (invoke-on-display arg
    (lambda ()
      (save-window-excursion
-       ;;(shell-command
        (apply
         'start-process
         "xterm"
         nil
-        "rxvt"
-                                        ;(concat
-                                        ;(mapconcat
-                                        ; 'identity
-
-                                        ;(list
-        ;;"rxvt"
-        "-geometry" "90x40"		;40 rows...
-        ;; no scrollbar!
-        "+sb"
-        ;; "-sl" "500"
-        ;; login shell? NO
-        "+ls"
-        "-j"
-        ;; "-ls"
-        ;; fixme: duplicated from ~/.Xdefaults
-        ;"-fn"
-        ;"-*-lucidatypewriter-medium-r-normal-*-18-*-*-*-*-*-*"
-        ;;"-title" "emacs"
-        ;; Old:
-                                        ;"xterm"; "/usr/local/bin/xterm" ;X11R6
-                                        ;"+sb -sl 5000"
-                                        ;"-geometry 90x52+10+0"
-                                        ;		  "-j -ls "
-                                        ;		  "-fn '-*-lucidatypewriter-medium-r-normal-*-20-*-*-*-*-*-*'"
-                                        ;		  "-bg black"
-                                        ;		  "-fg wheat"
-                                        ;(format "-title \"%s\"" (or title "exterm"))
+        "wezterm"
+        ;; "--config" "dpi=98"
+        "start"
+        ;; inside dired, it was ~/... and didn't work.
+        "--cwd" (expand-file-name default-directory)
         (if command
             (list "-e" command)
           ()))))
-                                        ;) ;;" ")
    ;; (bury-buffer)
    ;; put the buffer to auto-desctroy list...
    ;; Xterm survives, but  rxvt does not!
-                                        ;(run-with-timer 10 nil 'kill-buffer buffer) ; fixme: kill the bufer after ...
-     ))
+   ;; (run-with-timer 10 nil 'kill-buffer buffer) ; fixme: kill the bufer after ...
+   ))
 
 
 ;; fixme: in mmc-keys?
